fe2fad413c8ae6485e6469900aac446c0782df1c
TheKnightArgent <github@jsiii.com>
2013-09-04 20:09:55 -0700

    Commit from RubyMine

---
+++ lib/music_db/artist.rb
@@ -0,0 +1,16 @@
+require_relative 'yamlable'
+
+class MusicDB::Artist
+
+  include YAMLable
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def attributes
+    {name: @name}
+  end
+end
---
+++ lib/music_db/song.rb
@@ -0,0 +1,34 @@
+require_relative 'yamlable'
+
+class MusicDB::Song
+
+  include YAMLable
+
+  attr_accessor :title, :artist, :album, :track, :notes
+
+  def initialize(title, artist, album=nil, track=nil)
+    @title = title
+    @artist = artist
+    @album = album
+    @track = track
+  end
+
+
+  def to_s
+    output = "Title: #{@title}\n"
+    output << "Artist: #{@artist}\n"
+    output << "Album: #{@album}\n"
+    output << "Track: #{@track}\n"
+
+    output
+  end
+
+
+  def attributes
+    {title: @title,
+     artist: @artist,
+     album: @album,
+     track: @track
+    }
+  end
+end
